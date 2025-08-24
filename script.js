aa0b58df23f77eb47e41447fe832ce1d9b41eca6
Hamzah <ddsl45321@gmail.com>
2025-08-24 14:31:36 +0500

    updated links

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var typed=new Typed(".highlight",{
-    strings: ["Software Developer & Freelance","Frontend Developer","Web Developer","Mobile App Developer","UI/UX Designner","Microsoft Power Platform → PowerApps, Power Automate, SharePoint"],
-typeSpeed:100,
-backSpeed:100,
-backDelay:1000,
-loop:true
-})
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".highlight", {
+    strings: [
+      "Software Developer & Freelance",
+      "Frontend Developer",
+      "Web Developer",
+      "Mobile App Developer",
+      "UI/UX Designer",
+      "Microsoft Power Platform → PowerApps, Power Automate, SharePoint"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+});
